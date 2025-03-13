66fcbc5e0e27a62edc6f48b34e68ab2875199c9a
Vitor Lassen <vitor@lassen.com.br>
2025-03-13 02:07:03 -0300

    implemnt lazy load

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,36 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import DefaultHeader from './components/header/DefaultHeader.vue';
-import DefaultFooter from './components/footer/DefaultFooter.vue';
+import DefaultHeader from './components/header/DefaultHeader.vue'
+import DefaultFooter from './components/footer/DefaultFooter.vue'
 </script>
 
 <template>
-  <header class="d-flex flex-wrap ">
-    <v-banner
-    class="my-1 flex-1-0"
-    color="secondary"
-    icon="mdi-calendar-badge"
-    lines="one"
-    sticky
-    >
-    <v-banner-text>
-      <b>{{ $t('schedule') }}</b>
-    </v-banner-text>
-  </v-banner>
-
-  <DefaultHeader class="flex-1-1-100"/>
-
-
-
-  </header>
   <v-content>
-    <RouterView/>
+    <header class="d-flex flex-wrap">
+      <v-banner
+        class="my-1 flex-1-0"
+        color="secondary"
+        icon="mdi-calendar-badge"
+        lines="one"
+        sticky
+      >
+        <v-banner-text>
+          <b>{{ $t('schedule') }}</b>
+        </v-banner-text>
+      </v-banner>
 
+      <DefaultHeader class="flex-1-1-100" />
+    </header>
+
+    <v-lazy :options="{ threshold: 0.5 }" transition="scroll-y-reverse-transition">
+      <RouterView />
+    </v-lazy>
+    <v-btn class="whatsapp-button" icon="mdi-whatsapp" color="green"></v-btn>
+    <DefaultFooter />
   </v-content>
-  <v-btn  class="whatsapp-button" icon="mdi-whatsapp" color="green" ></v-btn>
-  <DefaultFooter/>
 </template>
 
 <style scoped>
-
 .whatsapp-button {
   position: fixed;
   bottom: 20px;

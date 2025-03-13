e4f54dc035c71fb6b4b8243b5b35c3fb52710a82
Vitor Lassen <vitor@lassen.com.br>
2025-03-12 23:28:50 -0300

    feat: menu

--- src/components/header/DefaultHeader.vue
+++ src/components/header/DefaultHeader.vue
@@ -1,67 +1,87 @@
 <template>
   <div>
-    <div class="d-flex align-center flex-md-row flex-column " style="height: fit-content;">
-      <div  class="pl-6">
-      <v-img
-        :width="170"
-        aspect-ratio="16/9"
-        cover
-        src="../src/assets/logo.png"
-      ></v-img>
-    </div>
+    <div class="d-flex align-center flex-md-row flex-column" style="height: fit-content">
+      <div class="pl-6">
+        <v-img :width="170" aspect-ratio="16/9" cover src="../src/assets/logo.png"></v-img>
+      </div>
       <MenuDropBox :title="$t('header.about')">
-              <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta eligendi beatae non aspernatur. Voluptatem dolores deserunt error consectetur in inventore veritatis id dolor ipsa, ullam maiores reiciendis laborum quod accusamus.</P>
+        <P
+          >Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta eligendi beatae non
+          aspernatur. Voluptatem dolores deserunt error consectetur in inventore veritatis id dolor
+          ipsa, ullam maiores reiciendis laborum quod accusamus.</P
+        >
       </MenuDropBox>
       <MenuDropBox :title="$t('header.english')">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste laboriosam, ratione, eum explicabo suscipit saepe inventore nisi modi odit, neque hic! Minima exercitationem magnam dolorum neque unde pariatur assumenda commodi.</p>
+        <ExchangeMenu
+          :title="$t('home.courses.for-kids.title')"
+          :description="$t('home.courses.for-kids.description')"
+          :button-text="$t('home.courses.for-kids.btn-learn-more')"
+          src="../src/assets/kid.png"
+        />
+        <ExchangeMenu
+          :title="$t('home.courses.for-adults.title')"
+          :description="$t('home.courses.for-adults.description')"
+          :button-text="$t('home.courses.for-adults.btn-learn-more')"
+          src="../src/assets/woman.png"
+          reverse
+        />
+        <ExchangeMenu
+          :title="$t('home.courses.for-teachers.title')"
+          :description="$t('home.courses.for-teachers.description')"
+          :button-text="$t('home.courses.for-teachers.btn-learn-more')"
+          src="../src/assets/teacher.png"
+        />
       </MenuDropBox>
       <MenuDropBox :title="$t('header.exchange.title')">
         <ExchangeMenu
-          :contry="$t('header.exchange.Slovakia.contry')"
+          :title="$t('header.exchange.Slovakia.contry')"
           :description="$t('header.exchange.Slovakia.description')"
           :button-text="$t('header.exchange.button')"
-          src="../src/assets/Flag_of_Slovakia.svg"/>
+          src="../src/assets/Flag_of_Slovakia.svg"
+        />
         <ExchangeMenu
-          :contry="$t('header.exchange.Ireland.contry')"
+          :title="$t('header.exchange.Ireland.contry')"
           :description="$t('header.exchange.Ireland.description')"
           :button-text="$t('header.exchange.button')"
           reverse
-          src="../src/assets/Flag_of_Ireland.svg"/>
+          src="../src/assets/Flag_of_Ireland.svg"
+        />
         <ExchangeMenu
-          :contry="$t('header.exchange.MaltaGozo.contry')"
+          :title="$t('header.exchange.MaltaGozo.contry')"
           :description="$t('header.exchange.MaltaGozo.description')"
           :button-text="$t('header.exchange.button')"
-          src="../src/assets/Flag_of_Slovakia.svg"/>
-
+          src="../src/assets/Flag_of_Slovakia.svg"
+        />
       </MenuDropBox>
       <MenuDropBox title="PARCERIAS">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste laboriosam, ratione, eum explicabo suscipit saepe inventore nisi modi odit, neque hic! Minima exercitationem magnam dolorum neque unde pariatur assumenda commodi.</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste laboriosam, ratione, eum
+          explicabo suscipit saepe inventore nisi modi odit, neque hic! Minima exercitationem magnam
+          dolorum neque unde pariatur assumenda commodi.
+        </p>
       </MenuDropBox>
 
-      <v-btn class="mr-6"  flat border rounded>Fale Conosco</v-btn>
+      <v-btn class="mr-6" flat border rounded>Fale Conosco</v-btn>
     </div>
-    <div  class="d-flex justify-space-between set-background-primary" >
+    <div class="d-flex justify-space-between set-background-primary">
       <div class="d-flex flex-row">
-        <p class="ma-2 pa-2 ml-6" style="color: white;">BB Blog</p>
-        <p class="ma-2 pa-2" style="color: white;">Teste seu nivel</p>
+        <p class="ma-2 pa-2 ml-6" style="color: white">BB Blog</p>
+        <p class="ma-2 pa-2" style="color: white">Teste seu nivel</p>
       </div>
       <div>
-        <MidiasSociais :size="40"/>
-
-     </div>
+        <MidiasSociais :size="40" />
+      </div>
     </div>
   </div>
-
 </template>
 
-<script  setup lang="ts">
-import MenuDropBox from './MenuDropBox.vue';
-import ExchangeMenu from './ExchangeMenu.vue';
-import MidiasSociais from '../MidiasSociais.vue';
+<script setup lang="ts">
+import MenuDropBox from './MenuDropBox.vue'
+import ExchangeMenu from './ItemMenu.vue'
+import MidiasSociais from '../MidiasSociais.vue'
 </script>
 
 <style scoped lang="scss">
-
 .set-background-primary {
   background-color: $primary;
 }
---
+++ src/components/header/ItemMenu.vue
@@ -0,0 +1,40 @@
+<template>
+  <div
+    class="d-flex justify-center aling-center menu"
+    :class="props.reverse ? 'flex-row-reverse' : 'flex-row'"
+  >
+    <v-img :src="props.src" aspect-ratio="16/9" max-width="200px" rounded></v-img>
+    <div class="ma-4 d-flex flex-column align-center justify-center" style="max-width: 600px">
+      <h3>{{ props.title }}</h3>
+      <p>{{ props.description }}</p>
+
+      <v-btn class="ma-2" color="secondary" :text="props.buttonText"></v-btn>
+    </div>
+  </div>
+  <v-divider class="ma-2"></v-divider>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const props = defineProps<{
+  reverse?: boolean
+  title: string
+  description: string
+  buttonText: string
+  src: string
+}>()
+</script>
+
+
+<style lang="scss" scoped>
+
+p{
+  font-size: 12px;
+}
+
+
+.menu:hover{
+  background-color: aqua;
+}
+</style>

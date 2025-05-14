d8a7ff1be314264a2d6ae1fc0d26f9da58a4aed9
Vitor Lassen <vitor@lassen.com.br>
2025-05-13 23:05:40 -0300

    feat: create teacher page

--- src/views/courses/TeachersView.vue
+++ src/views/courses/TeachersView.vue
@@ -1,14 +1,110 @@
 <template>
-  <div >
-    <h1>Teachers Courses</h1>
+  <div class="max-content-width center pa-8">
+    <div class="d-flex align-center center">
+      <div>
+        <h1>Education for Teachers</h1>
+        <p>
+          From our own experience, we know how difficult the teaching profession is. That is why we
+          want to invite you to a professional community for which education is a lifelong
+          enrichment. With us you will find new knowledge, skills and encouragement for further
+          pedagogical work. Like dozens of your teaching colleagues so far.
+        </p>
+      </div>
+      <v-img src="icones/one white teacher.png" min-width="200"> </v-img>
+    </div>
 
+    <p>
+      Our Education for Teachers programs are offered by our partner school The Bridge, in
+      Slovakia.<br />We offer online programs throughout the year and face-to-face programs mostly
+      in the European summer months.
+    </p>
+    <p>
+      "We apply a humanistically oriented philosophy of teaching, which we have developed with
+      globally recognized experts. They are the ones who lead our teachers in trainings, which take
+      place regularly every two months. At The Bridge, we believe that a teacher's personality is
+      key to students' academic success."
+    </p>
+  </div>
+  <div class="bg-bblue-v pr-6  pl-6 ">
+    <div class="d-flex align-center justify-space-around pa-6">
+      <h2>Study with the best</h2>
+      <v-img src="Stars.png" min-width="100" max-width="200"> </v-img>
+    </div>
+    <p class="ml-6 mr-6">
+      We offer specialized courses for teachers, drawing on the expertise of renowned international
+      lecturers and mentors. These professionals are leaders in the field of education, bringing
+      valuable insights and transformative teaching practices directly to you.
+    </p>
+  </div>
+  <div class="max-text-content-width center pa-6 mt-3 ">
+    <h2 class="ma-6 pb-6">Why choose our courses?</h2>
+
+    <ul class="teacher-list">
+      <li>
+        <b>Master Essential 21st-Century Topics</b><br />
+
+        <p>
+          Equip yourself with the tools and knowledge to address the real-life challenges your
+          students face today. Learn to guide them effectively by incorporating current, relevant
+          topics into your lessons using cutting-edge educational materials, digital tools, and
+          resources.
+        </p>
+      </li>
+      <li>
+        <b> Experience Modern, Interactive Teaching</b><br />
+
+        <p>
+          Dive into a world of dynamic, experiential teaching methods. These courses are designed to
+          rekindle your creativity, helping you engage students in meaningful ways and eliminate
+          monotony from your classroom.
+        </p>
+      </li>
+      <li>
+        <b> Inspire and Transform</b><br />
+
+        <p>
+          Beyond professional growth, our courses encourage personal development. Led by inspiring
+          mentors and featuring a thoughtfully designed leisure program, you'll gain space for
+          self-reflection and leave feeling empowered in a supportive and empathetic environment.
+        </p>
+      </li>
+    </ul>
+  </div>
+<div class="d-flex flex-column align-center max-content-width pa-6">
+    <p>
+      Take the next step in your teaching journey with The Bridge's transformative teacher education
+      courses, tailored to meet the demands of modern educators while inspiring a fresh perspective
+      in both your professional and personal life.
+    </p>
+
+<v-btn color="secondary" class="center ma-8" >I want to know more about these programs</v-btn>
+
+    <h2 class="pa-8 ma-6">The Bridge has been supporting teacher education for a long time</h2>
+
+    <p>They regularly organize international conferences for teachers and school principals on modern trends in education: Teaching for Tomorrow, Teachers' Day, ELT Forum, LAMSIG and others.</p>
+
+<p>For several years, they have been preparing in-person and online workshops and webinars for teachers with the aim of educating, motivating, inspiring and connecting educators who want to work on themselves.</p>
+
+<p>They are active in promoting the ideas of a modern educational ecosystem not only at our school. But also in the public, companies and in cooperation with several institutions.</p>
   </div>
 </template>
 
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <style scoped>
-
+.teacher-list {
+  list-style: circle !important;
+  list-style-type: circle !important;
+  list-style-position: outside !important;
+}
+.teacher-list li,
+.teacher-list li p {
+  list-style-type: circle !important;
+  margin-bottom: 10px;
+  padding-left: 20px;
+  text-align: justify;
+}
+p {
+  text-align: justify;
+}
 </style>

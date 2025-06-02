5c365887842e36eed237c6122445dae95fb1ff7a
Vitor Lassen <vitor@lassen.com.br>
2025-06-02 11:46:56 -0300

    remove margins social links

--- src/components/MidiasSociais.vue
+++ src/components/MidiasSociais.vue
@@ -1,7 +1,7 @@
 <template>
-    <a href="https://www.linkedin.com/company/bespokebridge"><v-icon icon="mdi-linkedin" :size="prop.size" class="ma-2" style="color: white;"/></a>
-    <a href="https://www.instagram.com/bespokebridge"><v-icon icon="mdi-instagram" :size="prop.size" class="ma-2" style="color: white;"/></a>
-    <a href="https://www.facebook.com/bespokebridge.co"><v-icon icon="mdi-facebook" :size="prop.size" class="ma-2 mr-8" style="color: white;"/></a>
+    <a href="https://www.linkedin.com/company/bespokebridge"><v-icon icon="mdi-linkedin" :size="prop.size" class="ma-1" style="color: white;"/></a>
+    <a href="https://www.instagram.com/bespokebridge"><v-icon icon="mdi-instagram" :size="prop.size" class="ma-1" style="color: white;"/></a>
+    <a href="https://www.facebook.com/bespokebridge.co"><v-icon icon="mdi-facebook" :size="prop.size" class="ma-1 mr-8" style="color: white;"/></a>
 </template>
 <script setup lang="ts">
 import { defineProps } from 'vue';

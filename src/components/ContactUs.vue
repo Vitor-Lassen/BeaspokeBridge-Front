ae7980eb52036382939f8e6a940dbd7b8a14c7b0
Vitor Lassen <vitor@lassen.com.br>
2025-11-16 19:14:39 -0300

    format email

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -6,20 +6,20 @@
     <v-text-field v-model="form.email" :label="$t('contact-us.form.email')" required></v-text-field>
     <v-radio-group v-if="props.showCourses" v-model="form.course">
       <p>Está interessado em qual curso? </p>
-      <v-radio label="Inglês Geral" />
-      <v-radio label="Inglês para Negócios" />
-      <v-radio label="Preparatório para Exames" />
-      <v-radio label="E.S.P. Inglês para propósitos específicos" />
-      <v-radio label="Educations for teachers" />
-      <v-radio label="BB Kids" />
-      <v-radio label="Inglês em empresas" />
+      <v-radio label="Inglês Geral" value="Inglês Geral"/>
+      <v-radio label="Inglês para Negócios" value="Inglês para Negócios"/>
+      <v-radio label="Preparatório para Exames"  value="Preparatório para Exames"/>
+      <v-radio label="E.S.P. Inglês para propósitos específicos" value="E.S.P. Inglês para propósitos específicos"/>
+      <v-radio label="Educations for teachers" value="Educations for teachers"/>
+      <v-radio label="BB Kids" value="BB Kids"/>
+      <v-radio label="Inglês em empresas" value="Inglês em empresas" />
 
     </v-radio-group>
     <v-radio-group v-if="props.showDestinations" v-model="form.destination">
       <p>Está interessado em ir para onde? </p>
-      <v-radio label="Eslováquia" />
-      <v-radio label="Irlanda" />
-      <v-radio label="Malta e Gozo" />
+      <v-radio label="Eslováquia" value="Eslováquia" />
+      <v-radio label="Irlanda" value="Irlanda"/>
+      <v-radio label="Malta e Gozo"  value="Malta e Gozo"/>
     </v-radio-group>
     <v-textarea v-model="form.message" :label="$t('contact-us.form.service')" rows="3" required></v-textarea>
    <div class="d-flex flex-column align-center">
@@ -53,6 +53,17 @@ const form = reactive({
   message:''
 })
 
+const subject = `<html><body>
+  <h2>Solicitação de contato do site</h2>
+  <p><strong>Nome:</strong> ${form.name}</p>
+  <p><strong>Email:</strong> ${form.email}</p>
+  <p><strong>Telefone:</strong> ${form.phone}</p>
+  ${form.course ? `<p><strong>Curso:</strong> ${form.course}</p>` : ''}
+  ${form.destination ? `<p><strong>Destino:</strong> ${form.destination}</p>` : ''}
+  <p><strong>Mensagem:</strong></p>
+  <p>${form.message}</p>
+</body></html>`;
+
 
 
 const sendEmail = () => {
@@ -63,13 +74,13 @@ const sendEmail = () => {
   },
   body: JSON.stringify({
     subject:  form.name + 'Solicitação de contato do site',
-    message: JSON.stringify(form)
+    message:  subject
   })
 })
 .then(response => response.json())
 .then(data => {
   if (data.success) {
-    // alert('E-mail enviado com sucesso!');
+    alert('Contato enviado com sucesso! Em breve entraremos em contato.');
   } else {
     alert('Erro ao enviar e-mail, por favor tente nos contactar diretamente pelo e-mail, ou whatsapp.');
   }
